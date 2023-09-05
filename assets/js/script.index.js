c106bc97544324c25de79a30d08205eb8bdb1fdb
analiviamartins <anamartins.senai564@gmail.com>
2023-09-05 08:40:03 -0300

    feat: function checkInputs

---
+++ assets/js/script.index.js
@@ -0,0 +1,34 @@
+console.log("tá funcionando!");
+
+function checkInputs(){
+    let nome= document.getElementById("name_input").value;
+    let fix= document.getElementById("fix_input").value;
+    let cell= document.getElementById("cell_input").value;
+    let foto= document.getElementById("foto_input").value;
+    let data= document.getElementById("date_input").value;
+    let email= document.getElementById("email_input").value;
+    let cep= document.getElementById("cep_input").value;
+    let city= document.getElementById("city_input").value;
+    let insta= document.getElementById("insta_input").value;
+    let git= document.getElementById("git_input").value;
+
+    console.log(nome);
+    console.log(fix);
+    console.log(cell);
+    console.log(foto);
+    console.log(data);
+    console.log(email);
+    console.log(cep);
+    console.log(city);
+    console.log(insta);
+    console.log(git);
+
+    if(nome == "" || fix == "" || cell == "" || foto == "" || data == "" || email == "" || cep == "" || city == "" || insta == "" || git == ""){
+        console.log("tá vazio")
+        return true;
+    }else{
+        console.log("Estão preenchidos")
+        return false;
+    }
+}
+
